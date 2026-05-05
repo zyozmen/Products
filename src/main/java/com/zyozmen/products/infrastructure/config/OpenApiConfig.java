ecadcd17cc15b4c535d28ffc7d52345e3d5faf75
zyozmen <61213472+zyozmen@users.noreply.github.com>
2026-05-04 19:20:55 -0500

    ajustes de documentacion

---
+++ src/main/java/com/zyozmen/products/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,53 @@
+package com.zyozmen.products.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${api.info.title}")
+    private String title;
+
+    @Value("${api.info.description}")
+    private String description;
+
+    @Value("${api.info.version}")
+    private String version;
+
+    @Value("${api.info.contact.name}")
+    private String contactName;
+
+    @Value("${api.info.contact.email}")
+    private String contactEmail;
+
+    @Value("${api.info.contact.url}")
+    private String contactUrl;
+
+    @Value("${api.info.license.name}")
+    private String licenseName;
+
+    @Value("${api.info.license.url}")
+    private String licenseUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                        .contact(new Contact()
+                                .name(contactName)
+                                .email(contactEmail)
+                                .url(contactUrl))
+                        .license(new License()
+                                .name(licenseName)
+                                .url(licenseUrl)));
+    }
+}
