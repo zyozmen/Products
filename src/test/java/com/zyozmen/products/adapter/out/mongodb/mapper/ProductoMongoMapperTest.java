69163b163afb9bb8e72a379fbba6bbb1826977c6
zyozmen <61213472+zyozmen@users.noreply.github.com>
2026-07-30 15:55:26 -0500

    ajuste de sonar

--- src/test/java/com/zyozmen/products/adapter/out/mongodb/mapper/ProductoMongoMapperTest.java
+++ src/test/java/com/zyozmen/products/adapter/out/mongodb/mapper/ProductoMongoMapperTest.java
@@ -18,7 +18,6 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
