2f31d6a1622fb07cbedb962cb3e2439bdc014fe2
zyozmen <61213472+zyozmen@users.noreply.github.com>
2026-08-06 20:38:25 -0500

    ajuste de memoria y verificacion de status

--- main.tf
+++ main.tf
@@ -246,7 +246,7 @@ resource "aws_ecs_task_definition" "app" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
-  memory                   = "512"
+  memory                   = "1024"
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
