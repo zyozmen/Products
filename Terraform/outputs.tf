3f77e8428681051d77a69acd245bf8a6aa7b1f4e
zyozmen <61213472+zyozmen@users.noreply.github.com>
2026-08-07 17:32:34 -0500

    ajuste para evitar duplicacion

--- Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "DNS del ALB para usar como Origen en CloudFront"
-  value       = aws_lb.api.dns_name
-}
-
-output "nat_gateway_public_ip" {
-  description = "IP PÚBLICA FIJA para agregar a la Whitelist / Network Access de MongoDB Atlas"
-  value       = aws_eip.nat.public_ip
-}
--- Terraform/ssm.tf
+++
@@ -1,3 +0,0 @@
-data "aws_ssm_parameter" "mongo_uri" {
-  name = "/prod/products-service/MONGO_URI"
-}
